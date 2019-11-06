760f176cc5b8e4f8f59b8c030cbf35652a118a6f
Marcin Stepien <marcinstepienpython@gmail.com>
2019-11-06 23:14:12 +0000

    added navbar and links

--- auctionPal/urls.py
+++ auctionPal/urls.py
@@ -24,12 +24,12 @@ from artifacts.views import ArtifactListView, ArtifactDetailView, ArtifactFeatur
 from .views import index, login_page, register_page
 
 urlpatterns = [
-    url(r'^$', index),
-    url(r'login/$', login_page),
-    url(r'register/$', register_page),
+    url(r'^$', index, name='index'),
+    url(r'login/$', login_page, name='login'),
+    url(r'register/$', register_page, name='register'),
     # ModelListView as a callable item
-    url(r'artifacts/$', ArtifactListView.as_view()),
-    url(r'featured/$', ArtifactFeaturedListView.as_view()),
+    url(r'artifacts/$', ArtifactListView.as_view(), name='artifacts'),
+    url(r'featured/$', ArtifactFeaturedListView.as_view(), name='featured'),
     # Checking the artifact primary key
     url(r'artifacts/(?P<pk>\d+)/$', ArtifactDetailView.as_view()),
     url(r'featured/(?P<pk>\d+)/$', ArtifactFeaturedDetailView.as_view()),
