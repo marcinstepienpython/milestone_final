75b5d69dd4dabc7d484f661cd3d70e19c7550a28
Marcin Stepien <marcinstepienpython@gmail.com>
2019-11-06 10:42:28 +0000

    added upload images functionality

--- artifacts/models.py
+++ artifacts/models.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import random
+import os
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 39999)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "artifacts/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
 
 class Artifact(models.Model):
     title = models.CharField(max_length=120)
@@ -15,6 +27,7 @@ class Artifact(models.Model):
     deadline = models.DateTimeField()
     seller = models.ForeignKey(User)
     sold = models.BooleanField(default=False)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.title
