c12ba47cb9b341934cc1b599a70dd58433f61fad
Marcin Stepien <marcinstepienpython@gmail.com>
2019-11-15 09:34:27 +0000

    added Q from django.db with multiple fields queries

--- search/views.py
+++ search/views.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.db.models import Q
 from django.views.generic import ListView
 from django.shortcuts import render
 
@@ -10,7 +10,8 @@ class SearchArtifactListView(ListView):
     template_name = 'search/view.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = super(SearchArtifactListView, self).get_context_data(*args,**kwargs)
+        context = super(SearchArtifactListView,
+                        self).get_context_data(*args, **kwargs)
         query = self.request.GET.get('q')
         context['query'] = query
         return context
@@ -20,5 +21,7 @@ class SearchArtifactListView(ListView):
         query = request.GET.get('q', None)
 
         if query is not None:
-            return Artifact.objects.filter(title__icontains=query)
+            lookups = Q(title__icontains=query) | Q(
+                description__icontains=query) | Q(year__icontains=query) | Q(origin__icontains=query)
+            return Artifact.objects.filter(lookups).distinct()
         return Artifact.objects.features()
