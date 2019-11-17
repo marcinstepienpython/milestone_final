73ef0b19ca7de3fea6057027a9dc18bf747c38f4
Marcin Stepien <marcinstepienpython@gmail.com>
2019-11-17 21:06:52 +0000

    created calculation of total items in cart

--- carts/models.py
+++ carts/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from artifacts.models import Artifact
+from django.db.models.signals import pre_save, post_save, m2m_changed
 from locale import str
 # Create your models here.
 
@@ -11,6 +12,23 @@ User = settings.AUTH_USER_MODEL
 
 
 class CartManager(models.Manager):
+
+    def new_or_get(self, request):
+        cart_id = request.session.get('cart_id', None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            # print('CartID exists')
+            cart_obj = qs.first()
+            if request.user.is_authenticated() and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Cart.objects.new_cart(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
+
     def new_cart(self, user=None):
         user_obj = None
         if user is not None:
@@ -31,3 +49,15 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+def pre_save_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        artifacts = instance.artifacts.all()
+        total = 0
+        for x in artifacts:
+
+            total += x.price
+        instance.total = total
+        instance.save()
+    
+m2m_changed.connect(pre_save_cart_receiver, sender=Cart.artifacts.through)
--- carts/views.py
+++ carts/views.py
@@ -5,21 +5,21 @@ from .models import Cart
 
 # Create your views here.
 
+
 def cart_create(user=None):
     cart_obj = Cart.objects.create(user=None)
-    print('New Cart created!')
+    # print('New Cart created!')
     return cart_obj
 
+
 def cart_home(request):
-    cart_id = request.session.get('cart_id', None)
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        print('CartID exists')
-        cart_obj = qs.first()
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new_cart(user=request.user)
-        request.session['cart_id'] = cart_obj.id
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    artifacts = cart_obj.artifacts.all()
+    total = 0
+    for x in artifacts:
+
+        total += x.price
+    print('total:', total)
+    cart_obj.total = total
+    cart_obj.save()
     return render(request, 'carts/home.html', {})
