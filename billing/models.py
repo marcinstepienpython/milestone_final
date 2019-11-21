ef2b52a4ed4a0b462edc0d4309aac39e7de95de1
Marcin Stepien <marcinstepienpython@gmail.com>
2019-11-21 11:21:07 +0000

    adding stripe part 1 installation customer_id stripe account setup

--- billing/models.py
+++ billing/models.py
@@ -2,10 +2,14 @@
 from __future__ import unicode_literals
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from accounts.models import GuestEmail
 
 User = settings.AUTH_USER_MODEL
+
+import stripe 
+stripe.api_key = 'sk_live_RQkA8NNySyPkwjT03Yyr6Us500b33NCs3B'
+
 
 class BillingProfileManager(models.Manager):
     def new_or_get(self, request):
@@ -29,6 +33,7 @@ class BillingProfile(models.Model):
     email = models.EmailField()
     active = models.BooleanField(default=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    customer_id= models.CharField(max_length=120, null=True, blank=True)
 
     objects = BillingProfileManager()
 
@@ -38,6 +43,14 @@ class BillingProfile(models.Model):
     
 
 # Create billing profile after a new User has been created.
+def billing_profile_created_receiver(sender, instance, *args, **kwargs):
+    if not instance.customer_id and instance.email:
+        customer = stripe.Customer.create(email=instance.email)
+        print(customer)
+        instance.customer_id = customer.id
+        
+
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
 
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
